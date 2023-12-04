0ab3dbcf195ab44744cb50a5c3aea497745c5080
JinSung Kang <gogs14@hotmail.com>
2023-12-04 01:38:23 -0500

    More clean up

--- 4/4.py
+++ 4/4.py
@@ -6,6 +6,7 @@ X = X_real
 
 card_counter = collections.defaultdict(int)
 part1_sum = 0
+part2_sum = 0
 
 for i in range(len(X)):
     win, number = X[i].split(':')[1].split('|')
@@ -16,21 +17,20 @@ for i in range(len(X)):
     number_set = set(number)
     winning_numbers = win_set.intersection(number_set)
 
+    # part1: Calculate points
+    if len(winning_numbers):
+        part1_sum += pow(2, len(winning_numbers) - 1)
+
+    # part2:
     # Since you're processing the card, you must have one card
     card_counter[i] += 1
     for j in range(len(winning_numbers)):
         win_pos = i + j + 1
         if win_pos < len(X):
             card_counter[win_pos] += card_counter[i]
-    
-    # Calculate points
-    if len(winning_numbers):
-        part1_sum += pow(2, len(winning_numbers) - 1)
 
-print(part1_sum)
-
-part2_sum = 0
 for k,v in card_counter.items():
     part2_sum += v 
 
+print(part1_sum)
 print(part2_sum)
