9ceacb903af2f147c951b1c33d71e6d3e6b206f8
JinSung Kang <gogs14@hotmail.com>
2023-12-09 02:12:39 -0500

    clean up

--- 9/9.py
+++ 9/9.py
@@ -14,16 +14,15 @@ def take_diff(row):
     
     return rows
 
-def get_diff(diff_list):
+def get_increment_prediction(diff_list):
     for i in reversed(range(len(diff_list))):
-        if i == len(diff_list) - 1:
-            fill_value = 0
-        else:
-            fill_value = diff_list[i][-1] + diff_list[i+1][-1]
+        next = 0
+        if i != len(diff_list) - 1:
+            next = diff_list[i][-1] + diff_list[i+1][-1]
         
-        diff_list[i].append(fill_value)
+        diff_list[i].append(next)
     
-    return fill_value
+    return next 
 
 part_1_sum = 0
 part_2_sum = 0
@@ -32,8 +31,8 @@ for x in X:
     row = [int(xp) for xp in x.split()]
     part2_row = list(reversed(row))
 
-    part_1_sum += get_diff(take_diff(row)) + row[-1]
-    part_2_sum += get_diff(take_diff(part2_row)) + part2_row[-1]
+    part_1_sum += get_increment_prediction(take_diff(row)) + row[-1]
+    part_2_sum += get_increment_prediction(take_diff(part2_row)) + part2_row[-1]
 
 print('part 1:', part_1_sum)
 print('part 2:', part_2_sum)
