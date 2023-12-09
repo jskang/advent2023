6d09e1dca2498b5e1dfc8820b331986e6be6d003
JinSung Kang <gogs14@hotmail.com>
2023-12-09 02:05:14 -0500

    Clean up

--- 9/9.py
+++ 9/9.py
@@ -1,30 +1,21 @@
 from operator import sub
-import collections
-import re
 
 X_real= [l.strip() for l in open('9-input')]
 X_test = [l.strip() for l in open('9-input-test')]
 
-X = X_test
+X = X_real
 
 def take_diff(row):
-    rows = []
     diff_row = list(map(sub, row[1:], row))
 
-    rows.append(diff_row)
+    rows = [diff_row]
     if not all(v== 0 for v in diff_row):
         rows.extend(take_diff(diff_row))
     
     return rows
 
-part_1_sum = 0
-for x in X:
-    row = [int(xp) for xp in x.split()]
-    diff_list = take_diff(row)
-    print(diff_list)
-    target_length = len(row) 
-
-    for i in range(len(diff_list) - 1, -1, -1):
+def get_diff(diff_list):
+    for i in reversed(range(len(diff_list))):
         if i == len(diff_list) - 1:
             fill_value = 0
         else:
@@ -32,27 +23,17 @@ for x in X:
         
         diff_list[i].append(fill_value)
     
-    part_1_sum += fill_value + row[-1]
+    return fill_value
 
+part_1_sum = 0
 part_2_sum = 0
+
 for x in X:
     row = [int(xp) for xp in x.split()]
-    diff_list = take_diff(row)
-    target_length = len(row) 
-    
-    for i in range(len(diff_list) - 1, -1, -1):
-        if i == len(diff_list) - 1:
-            fill_value = 0
-        else:
-            fill_value = diff_list[i][0] - diff_list[i+1][0]
-        
-        diff_list[i].insert(0, fill_value)
-    
-    part_2_sum += row[0] - fill_value
+    part2_row = list(reversed(row))
+
+    part_1_sum += get_diff(take_diff(row)) + row[-1]
+    part_2_sum += get_diff(take_diff(part2_row)) + part2_row[-1]
 
 print('part 1:', part_1_sum)
 print('part 2:', part_2_sum)
-
-
-
-
