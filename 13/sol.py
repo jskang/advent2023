0e94c837aae4790f41fd2a68e74ce1e6766ffeca
JinSung Kang <gogs14@hotmail.com>
2023-12-13 01:25:29 -0500

    clean up

--- 13/sol.py
+++ 13/sol.py
@@ -1,10 +1,10 @@
-X_real= [l.strip() for l in open('input.txt')]
+X_real = [l.strip() for l in open('input.txt')]
 X_test = [l.strip() for l in open('input-test.txt')]
 
 L = X_real
 
+# Need to find a cleaner way to parse the double line input
 maps = []
-
 sub_map = []
 for l in L:
     if not l:
@@ -22,11 +22,10 @@ def matcher(m, multiplier, part2=False):
         right = m[i:i+check_range]
 
         if part2:
-            comp_all = []
-            for l, r in zip(left, right):
-                comparison = [lc == rc for lc, rc in zip(l, r)]
-                comp_all.extend(comparison)
-            
+            # Only one smudge needs to be fixed, the two images should only contain one difference
+            # We don't care where the smudge is, just that there is one
+            comp_all = [lc == rc for l, r in zip(left, right) for lc, rc in zip(l, r)]
+
             if len(comp_all) - sum(comp_all) == 1:
                 points += i * multiplier
         else:
@@ -37,21 +36,17 @@ def matcher(m, multiplier, part2=False):
 
 part_1 = 0
 part_2 = 0
+
 for m in maps:
-    
     part_1 += matcher(m, 100)
     part_2 += matcher(m, 100, True)
 
     # flip row and columns
-    columns = []
-    for j in range(len(m[0])):
-        row = []
-        for i in range(len(m)):   
-            row.append(m[i][j])
-        columns.append(''.join(row))
+    # columns = list(map(list, zip(*m)))
+    columns = [*zip(*m)]
 
     part_1 += matcher(columns, 1)
     part_2 += matcher(columns, 1, True)
     
 print('part 1:', part_1)
-print('part 1:', part_2)
+print('part 2:', part_2)
